9aaf47a231d0e13f8f0b776408cff6b141c9f3ac
Stefano Gallorini <himens@gmail.com>
2021-08-03 23:14:32 +0200

    Minor change.

--- src/test.cpp
+++ src/test.cpp
@@ -43,10 +43,7 @@ std::shared_ptr<Command> Test::get_command(const std::string name, const std::ve
   if (name == "\\set_thrust") cmd = std::make_shared<SetThrustCmd>();
   else if (name == "\\insert_pds") cmd = std::make_shared<InsertPdsCmd>();
   else if (name == "\\check") cmd = std::make_shared<CheckCmd>();
-  else if (_user_command_map.count(name) > 0) 
-  {
-    cmd = std::make_shared<UserCmd>(_user_command_map[name]); // check if object is copied!!!
-  }
+  else if (_user_command_map.count(name) > 0) cmd = std::make_shared<UserCmd>(_user_command_map[name]); 
   else 
   {
     std::cout << "ERROR: Unknown command '" << name << "'! \n";
