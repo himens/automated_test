2da1d8fcc40f63fadfaded6498cef40e0bb1becc
Stefano Gallorini <himens@gmail.com>
2021-08-20 01:09:54 +0200

    Minor changes.

--- src/test.cpp
+++ src/test.cpp
@@ -26,44 +26,6 @@ void Test::run()
 }
 
 
-/***************/
-/* Get command */
-/***************/
-std::shared_ptr<Command> Test::get_command(const std::string name)
-{
-  std::shared_ptr<Command> cmd = nullptr;
-
-  auto it = std::find_if(_user_commands.begin(), _user_commands.end(), 
-      [&] (UserCmd cmd) { return name == cmd.get_name(); });
-
-  if (name == "set_thrust") cmd = std::make_shared<SetThrustCmd>();
-  else if (name == "insert_pds") cmd = std::make_shared<InsertPdsCmd>();
-  else if (name == "check") cmd = std::make_shared<CheckCmd>();
-  else if (it != _user_commands.end()) 
-  {
-    auto usr_cmd = std::make_shared<UserCmd>(*it);
-
-    // allocate new commands (otherwise the same of *it will be used...)
-    std::vector<std::shared_ptr<Command>> commands = {};
-    for (auto cmd : usr_cmd->get_commands())
-    {
-      auto c = get_command( cmd->get_name() );
-      c->set_args( cmd->get_args() );
-      commands.push_back(c);
-    }
-
-    usr_cmd->set_commands(commands);
-    cmd = usr_cmd;
-  }
-  else 
-  {
-    throw Error("Unknown command '" + name + "'!");
-  }
-
-  return cmd;
-}
-
-
 /*******************/
 /* Parse test file */
 /*******************/
@@ -179,7 +141,7 @@ void Test::parse_test(const std::string filename)
 
 	replace_variable_with_val(cmd_args);
 
-	auto cmd = get_command(cmd_name);
+	auto cmd = make_command(cmd_name);
 	cmd->set_args(cmd_args);
 	commands.push_back(cmd);
       } 
@@ -225,7 +187,7 @@ void Test::parse_test(const std::string filename)
 	
 	replace_variable_with_val(cmd_args);
 
-	auto cmd = get_command(cmd_name);
+	auto cmd = make_command(cmd_name);
 	cmd->set_args(cmd_args);
 	commands.push_back(cmd);
       } 
